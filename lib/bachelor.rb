19222b4b0979a7b4e7fab968780ffe0bbb5465b1
Weijun Huang <wjhuang0726@gmail.com>
2018-12-13 20:48:06 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,15 @@
+require 'json'
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  return data.class
+  
 end
 
 def get_contestant_name(data, occupation)
   # code here
+
+
 end
 
 def count_contestants_by_hometown(data, hometown)
