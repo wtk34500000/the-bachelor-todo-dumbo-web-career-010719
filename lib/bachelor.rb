faec9cf5f64a0374d7ddf6f5aa02d0e5c26f0929
Weijun Huang <wjhuang0726@gmail.com>
2018-12-13 21:15:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,24 @@
+
+
 def get_first_name_of_season_winner(data, season)
   # code here
+    for data in data[season.to_sym]
+      if data[:status] == "Winner"
+      return data[:name]
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |show_season, value|
+       for item in value 
+        if item[:occupation] == occupation
+         return item[:name]
+        end
+    end
+  end
+
 end
 
 def count_contestants_by_hometown(data, hometown)
