b8bc1212f4b49f5efe512624bd520ea60ed59088
Weijun Huang <wjhuang0726@gmail.com>
2018-12-13 21:11:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,20 @@
+
+
 def get_first_name_of_season_winner(data, season)
   # code here
+    for data in data[season.to_sym]
+      if data[:status] == "Winner"
+      return data[:name][0..4]
+      end
+      
+    end
+  
 end
 
 def get_contestant_name(data, occupation)
   # code here
+
+
 end
 
 def count_contestants_by_hometown(data, hometown)
